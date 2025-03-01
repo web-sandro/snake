22620f12eefdb3032ac625a25b9f55a803536ee8
Software <sandrogomesoficial@gmail.comcd>
2025-03-01 14:06:56 -0300

    ok

--- models/models/gameModel.js
+++
@@ -1,9 +0,0 @@
-exports.initializeGame = () => {
-    return {
-        dotSize: 20,
-        gapSize: 3,
-        colors: ["yellow", "green", "purple", "red"],
-        colorValues: { yellow: 1, green: 2, purple: 3, red: 0 },
-        speed: 500,
-    };
-};
