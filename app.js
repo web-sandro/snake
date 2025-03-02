d17caadc72da4761349262c90b4a5f0f2fd6a2ca
Software <sandrogomesoficial@gmail.comcd>
2025-03-02 04:09:45 -0300

    ok

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var path = require('path');
-// var indexRouter = require('./routes/indexRoutes');
 var gameRoutes = require("./routes/gameRoutes");
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -12,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
 app.use("/", gameRoutes);
 
 module.exports = app;
