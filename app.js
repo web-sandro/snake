fb85b519da5538060fc0bd8fc0039377c4236b34
Software <sandrogomesoficial@gmail.comcd>
2025-03-02 03:33:22 -0300

    error

--- app.js
+++ app.js
@@ -22,40 +22,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-// Rota de exemplo
-app.get('/', (req, res) => {
-    res.send('Página inicial');
-});
-
-// Middleware para tratar erros
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-
-    // Renderiza a página de erro, passando 'mensagem' e 'error'
-    res.render('error', {
-        mensagem: 'Ocorreu um erro inesperado.',  // Mensagem de erro personalizada
-        error: err                            // O objeto de erro
-    });
-});
-
-
-
 module.exports = app;
